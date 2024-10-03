b1c930eae7ed5b0dfb3f7331e7c29c9fe45e1e3a
Srinivasarao245 <srinivasaraobagam@gmail.com>
2024-10-03 18:43:27 +0530

    Jsonserver

---
+++ Jsonser12p.js
@@ -0,0 +1,62 @@
+let pizzas=[
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2oXZTqj2csZlVAKkc7LWSyc92Y9EgK8yI_g&s",
+        "name": "Margherita Pizza",
+        "type": "Vegetarian",
+        "price": 12.99
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT03K3j61ELhhHOWKzug5lm7AY8bxOHk4UExQ&s",
+        "name": "Pepperoni Pizza",
+        "type": "Meat",
+        "price": 14.99
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOQy53KPD6AGtUG2cdI-v4oR0Xo5s0E7y3AA&s",
+        "name": "Veggie Supreme",
+        "type": "Vegetarian",
+        "price": 13.49
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3lVwci-ripd9xHMMGMmcSyJNTWevo2MmnLQ&s",
+        "name": "BBQ Chicken Pizza",
+        "type": "Meat",
+        "price": 15.99
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLDnYIwbcwFU0x3-f7Hv1C2VZCBltltrnsXA&s",
+        "name": "Seafood Pizza",
+        "type": "Specialty",
+        "price": 18.99
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7LCDxMgcE4ipIvUx23-7d69zWFVB7CFMRFw&s",
+        "name": "Hawaiian Pizza",
+        "type": "Meat",
+        "price": 13.99
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDZ1iyPUDZrmZfd99rRistt6oOXSTws2vNUw&s",
+        "name": "Pepperoni Mushroom",
+        "type": "Meat",
+        "price": 15.49
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4R8ZYhmuZnrq3MNTRpSuYrdfTx-of4sArFg&s",
+        "name": "Chicken Pizza",
+        "type": "Meat",
+        "price": 16.99
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOJKo9ygz6TwQOoe_Te2-yF9TL8utazxl5uw&s",
+        "name": "Pesto VeggiePizza",
+        "type": "Vegetarian",
+        "price": 14.49
+    },
+    {
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3kxloD1xO92Y6df0hyaXn9v7fXhvhIyt2NhFstOr2ay-1HraKu_5PXcdnb6Vq1242jBA&usqp=CAU",
+        "name": "White Pizza",
+        "type": "Vegetarian",
+        "price": 13.99
+    }
+]
